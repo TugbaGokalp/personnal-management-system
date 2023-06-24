3ede5025a827b5686eb5d31d049d2f1042fe6277
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-06-24 22:39:35 +0300

    authorization added with json web token

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -3,9 +3,11 @@
 import express from 'express';
 import * as pageController from "../controllers/pageController.js"; // default olarak export edilmedigi icin bu sekide aldik
 
+import * as authMiddleware from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
-router.route("/").get(pageController.getIndexPage)
+router.route("/").get(authMiddleware.authenticateToken, pageController.getIndexPage)
 router.route("/about").get(pageController.getAboutPage);
 router.route("/register").get(pageController.getRegisterPage);
 router.route("/login").get(pageController.getLoginPage);
